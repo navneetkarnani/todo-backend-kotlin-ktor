a44457d5fbfa882bdee06593acb4f75138ba5182
Navneet Karnani <navneet@mandraketech.in>
2018-10-12 00:11:53 +0530

    First version of the web application

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,19 @@
+package backend.todo.ktor
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun Application.mymodule() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!", ContentType.Text.Plain)
+        }
+        get("/demo") {
+            call.respondText("HELLO WORLD!")
+        }
+    }
+}
