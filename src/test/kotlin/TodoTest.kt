30dd58680de1e45ba0a2550f2329dd5ea26098dd
Navneet Karnani <navneet@mandraketech.in>
2018-10-14 01:05:37 +0530

    Tests hit the routes

--- src/test/kotlin/TodoTest.kt
+++ src/test/kotlin/TodoTest.kt
@@ -1,7 +1,9 @@
 package backend.todo.ktor
 
 import io.ktor.application.Application
+import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
 import kotlinx.serialization.json.JSON
 import org.junit.Test
@@ -11,15 +13,17 @@ import kotlin.test.assertNotNull
 class TodoTest {
     @Test
     fun getAllTest() = withTestApplication(Application::main) {
-        with( handleRequest { "/" }) {
+        with( handleRequest(HttpMethod.Get, "/")) {
             assertEquals(HttpStatusCode.OK, response.status())
-            assertEquals("{}", response.content)
+            assertNotNull(response.content);
+            val todoList = response.content?.let { JSON.parse<ArrayList<Todo>>(it) }
+            assertNotNull(todoList);
         }
     }
 
     @Test
-    fun getAllOneTest() = withTestApplication(Application::main) {
-        with( handleRequest { "/1" }) {
+    fun getOneTest() = withTestApplication(Application::main) {
+        with( handleRequest(HttpMethod.Get,"/1")) {
             assertEquals(HttpStatusCode.OK, response.status())
             assertNotNull(response.content);
             val todo = response.content?.let { JSON.parse<Todo>(it) }
